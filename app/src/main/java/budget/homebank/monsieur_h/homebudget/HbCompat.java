766e16465003b4cbee1db8c804aba1dd6276e1fc
monsieur_h <ray.hubert@riseup.net>
2017-02-01 23:16:30 +0100

    init

---
+++ app/src/main/java/budget/homebank/monsieur_h/homebudget/HbCompat.java
@@ -0,0 +1,43 @@
+package budget.homebank.monsieur_h.homebudget;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+/**
+ * Created by hub on 01/02/17.
+ */
+
+public class HbCompat {
+
+    public static long dateToJulian(Date date) {
+
+        GregorianCalendar calendarDate = new GregorianCalendar();
+        calendarDate.setTime(date);
+
+        Calendar calendarBegin = new GregorianCalendar(1, GregorianCalendar.JANUARY, 1);
+
+        long millisecondsDate = calendarDate.getTimeInMillis();
+        long millisecondsBegin = calendarBegin.getTimeInMillis();
+        long diff = millisecondsDate - millisecondsBegin;
+        long diffDays = diff / (24 * 60 * 60 * 1000) - 1;
+
+        return diffDays;
+    }
+
+    public static Date julianToDate(long date) {
+        GregorianCalendar calendarBegin = new GregorianCalendar(1, GregorianCalendar.JANUARY, 1);
+        long millisecondsBegin = calendarBegin.getTimeInMillis();
+
+        long diff = date * (24 * 60 * 60 * 1000);
+        long millisecondsDate = diff + millisecondsBegin;
+        int days = (int) (millisecondsDate / (24 * 60 * 60 * 1000));
+
+        GregorianCalendar calendarDate = new GregorianCalendar();
+        calendarDate.setTimeInMillis(0);
+        calendarDate.add(GregorianCalendar.DAY_OF_MONTH, days + 2);
+
+        return calendarDate.getTime();
+    }
+
+}
