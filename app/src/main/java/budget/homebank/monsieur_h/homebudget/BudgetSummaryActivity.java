293cd99efd9f5c91b6abf827542b6f690810c6f5
ahub <ray.hubert@riseup.net>
2017-02-09 20:22:40 +0100

    renames activity_main instead of tmp

--- app/src/main/java/budget/homebank/monsieur_h/homebudget/BudgetSummaryActivity.java
+++ app/src/main/java/budget/homebank/monsieur_h/homebudget/BudgetSummaryActivity.java
@@ -40,7 +40,7 @@ public class BudgetSummaryActivity extends AppCompatActivity implements OnClickL
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tmp);
+        setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
